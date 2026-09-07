8db7489473d1853905fad66094c2f08e051300d3
rltgjqmtkdydwk <slvrcods22@gmail.com>
2026-09-07 13:12:45 +0900

    프로젝트 생성

---
+++ hello/src/main/java/net/skhu/controller/HomeController.java
@@ -0,0 +1,15 @@
+package net.skhu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "좋은 아침");
+        return "index";
+    }
+}
