f70e583c6d35f4bd1ecf34a72d407a8a361c9733
rltgjqmtkdydwk <slvrcods22@gmail.com>
2026-09-07 13:22:26 +0900

    실습

--- hello/src/main/java/net/skhu/controller/HomeController.java
+++ hello/src/main/java/net/skhu/controller/HomeController.java
@@ -1,5 +1,7 @@
 package net.skhu.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ public class HomeController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("message", "좋은 아침");
+        model.addAttribute("now", new Date());
         return "index";
     }
 }
