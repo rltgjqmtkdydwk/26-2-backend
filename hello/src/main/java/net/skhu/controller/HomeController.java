bc040920cb88ff9e3dfb4e6f7d72c80149b9a356
rltgjqmtkdydwk <slvrcods22@gmail.com>
2026-09-07 13:21:05 +0900

    Spring Web MVC

--- hello/src/main/java/net/skhu/controller/HomeController.java
+++ hello/src/main/java/net/skhu/controller/HomeController.java
@@ -1,7 +1,5 @@
 package net.skhu.controller;
 
-import java.util.Date;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +10,6 @@ public class HomeController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("message", "좋은 아침");
-        model.addAttribute("now", new Date());
         return "index";
     }
 }
