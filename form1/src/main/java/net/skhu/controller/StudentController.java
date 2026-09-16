feff583935bfcd8f3782b8470b7d440766541166
rltgjqmtkdydwk <slvrcods22@gmail.com>
2026-09-16 12:22:56 +0900

    03 StudentController.java 생성

---
+++ form1/src/main/java/net/skhu/controller/StudentController.java
@@ -0,0 +1,49 @@
+package net.skhu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.skhu.dto.Student;
+
+@Controller
+@RequestMapping("student")
+public class StudentController {
+
+    @GetMapping("edit")
+    public String edit(Model model) {
+        Student student = new Student();
+        student.setName("홍길동");
+        model.addAttribute("student", student);
+        return "student/edit";
+    }
+
+    @PostMapping("edit")
+    public String edit(Model model, Student student) {
+        try {
+            if (StringUtils.hasText(student.getStudentNo()) == false) {
+				throw new Exception("학번을 입력하세요");
+			}
+            if (StringUtils.hasText(student.getName()) == false) {
+				throw new Exception("이름을 입력하세요");
+			}
+
+            // student 객체를 DB에 저장하는 구현 생략
+            System.out.println(student.getStudentNo() + " " + student.getName());
+            return "redirect:list";
+        }
+        catch (Exception ex) {
+            model.addAttribute("errorMsg", ex.getMessage());
+            return "student/edit";
+        }
+    }
+
+    @GetMapping("list")
+    public String list() {
+        // 학색 목록을 DB에서 조회해서 뷰에 전달하는 코드 생략
+        return "student/list";
+    }
+}
