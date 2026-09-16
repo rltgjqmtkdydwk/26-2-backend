e357b503627ac365d37395724a1322f5497546b6
rltgjqmtkdydwk <slvrcods22@gmail.com>
2026-09-16 12:22:22 +0900

    03 Student.java 생성

---
+++ form1/src/main/java/net/skhu/dto/Student.java
@@ -0,0 +1,22 @@
+package net.skhu.dto;
+
+public class Student {
+    String studentNo;
+    String name;
+
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
